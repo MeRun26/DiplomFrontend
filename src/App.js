486ca74f40a27f63578a2a14770834817da96372
Miran <Emilbekov.m091@mail.ru>
2024-05-10 20:22:32 +0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+const Div = styled.div`
+text-align: center;
+`;
+
+export const App = () => {
+  return (
+    <Div>
+      <i className="fa fa-calendar"/>
+      <div>
+        123
+      </div>
+    </Div>
+  );
+}
